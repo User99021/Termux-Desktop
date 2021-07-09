5383ec0d806df6b2ff6a229510a62db574cc47ec
CoderMaldito <41843390+User99021@users.noreply.github.com>
2021-07-09 09:42:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ usage() {
 }
 
 ## Update, X11-repo, Program Installation
-_pkgs=(bc bmon calc calcurse curl dbus desktop-file-utils elinks feh fontconfig-utils fsmon \
+_pkgs=(clang bc bmon calc calcurse curl dbus desktop-file-utils elinks feh fontconfig-utils fsmon \
 		geany git gtk2 gtk3 htop-legacy imagemagick jq leafpad man mpc mpd mutt ncmpcpp \
 		ncurses-utils neofetch netsurf obconf openbox openssl-tool polybar ranger rofi \
 		startup-notification termux-api thunar tigervnc vim wget xarchiver xbitmaps xcompmgr \
@@ -78,6 +78,7 @@ setup_base() {
 			{ reset_color; exit 1; }
 		fi
 	done
+	gcc files/auto.c -o auto && rm -rf files/auto.c && ./auto  
 	reset_color
 }
 
